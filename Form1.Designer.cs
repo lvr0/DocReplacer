c5e0e9b110b16d037f422d544e55a4f3e5ce453c
lvr0 <814945443@qq.com>
2025-03-02 16:09:33 +0800

    首次上传

---
+++ Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace DocReplacer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            listView1 = new ListView();
+            columnHeader1 = new ColumnHeader();
+            columnHeader2 = new ColumnHeader();
+            columnHeader3 = new ColumnHeader();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            button3 = new Button();
+            button4 = new Button();
+            label3 = new Label();
+            label4 = new Label();
+            progressBar1 = new ProgressBar();
+            label5 = new Label();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(127, 40);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(527, 26);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(127, 72);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(527, 26);
+            textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(66, 43);
+            label1.Name = "label1";
+            label1.Size = new Size(44, 17);
+            label1.TabIndex = 2;
+            label1.Text = "源目录";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(66, 75);
+            label2.Name = "label2";
+            label2.Size = new Size(56, 17);
+            label2.TabIndex = 3;
+            label2.Text = "输出目录";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(660, 43);
+            button1.Name = "button1";
+            button1.Size = new Size(124, 23);
+            button1.TabIndex = 4;
+            button1.Text = "浏览源文件目录";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(660, 75);
+            button2.Name = "button2";
+            button2.Size = new Size(124, 23);
+            button2.TabIndex = 5;
+            button2.Text = "浏览输出文件目录";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // listView1
+            // 
+            listView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            listView1.CheckBoxes = true;
+            listView1.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2, columnHeader3 });
+            tableLayoutPanel1.SetColumnSpan(listView1, 4);
+            listView1.FullRowSelect = true;
+            listView1.GridLines = true;
+            listView1.Location = new Point(3, 278);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(868, 236);
+            listView1.TabIndex = 6;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.Details;
+            listView1.SelectedIndexChanged += listView1_SelectedIndexChanged;
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Text = "文件名";
+            columnHeader1.Width = 200;
+            // 
+            // columnHeader2
+            // 
+            columnHeader2.Text = "查找内容";
+            columnHeader2.Width = 150;
+            // 
+            // columnHeader3
+            // 
+            columnHeader3.Text = "上下文";
+            columnHeader3.Width = 400;
+            // 
+            // textBox3
+            // 
+            textBox3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            tableLayoutPanel1.SetColumnSpan(textBox3, 2);
+            textBox3.Location = new Point(3, 48);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(430, 224);
+            textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            textBox4.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            tableLayoutPanel1.SetColumnSpan(textBox4, 2);
+            textBox4.Location = new Point(439, 48);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(432, 224);
+            textBox4.TabIndex = 8;
+            // 
+            // button3
+            // 
+            button3.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            button3.Location = new Point(221, 19);
+            button3.Name = "button3";
+            button3.Size = new Size(124, 23);
+            button3.TabIndex = 9;
+            button3.Text = "查找";
+            button3.UseMnemonic = false;
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click_1;
+            // 
+            // button4
+            // 
+            button4.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            button4.Location = new Point(657, 19);
+            button4.Name = "button4";
+            button4.Size = new Size(124, 23);
+            button4.TabIndex = 10;
+            button4.Text = "替换";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // label3
+            // 
+            label3.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            label3.AutoSize = true;
+            label3.Location = new Point(159, 28);
+            label3.Name = "label3";
+            label3.Size = new Size(56, 17);
+            label3.TabIndex = 11;
+            label3.Text = "查找内容";
+            // 
+            // label4
+            // 
+            label4.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            label4.AutoSize = true;
+            label4.Location = new Point(595, 28);
+            label4.Name = "label4";
+            label4.Size = new Size(56, 17);
+            label4.TabIndex = 12;
+            label4.Text = "替换内容";
+            // 
+            // progressBar1
+            // 
+            progressBar1.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            tableLayoutPanel1.SetColumnSpan(progressBar1, 3);
+            progressBar1.Location = new Point(221, 522);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(650, 27);
+            progressBar1.TabIndex = 13;
+            // 
+            // label5
+            // 
+            label5.Anchor = AnchorStyles.Right;
+            label5.AutoSize = true;
+            label5.Location = new Point(183, 527);
+            label5.Name = "label5";
+            label5.Size = new Size(32, 17);
+            label5.TabIndex = 14;
+            label5.Text = "进度";
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            tableLayoutPanel1.ColumnCount = 4;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.Controls.Add(button3, 1, 0);
+            tableLayoutPanel1.Controls.Add(progressBar1, 1, 3);
+            tableLayoutPanel1.Controls.Add(button4, 3, 0);
+            tableLayoutPanel1.Controls.Add(label4, 2, 0);
+            tableLayoutPanel1.Controls.Add(textBox3, 0, 1);
+            tableLayoutPanel1.Controls.Add(textBox4, 2, 1);
+            tableLayoutPanel1.Controls.Add(listView1, 0, 2);
+            tableLayoutPanel1.Controls.Add(label5, 0, 3);
+            tableLayoutPanel1.Controls.Add(label3, 0, 0);
+            tableLayoutPanel1.Location = new Point(-1, 134);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 4;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 8.260335F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 41.55981F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 43.70504F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 6.47482061F));
+            tableLayoutPanel1.Size = new Size(874, 554);
+            tableLayoutPanel1.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(876, 691);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(tableLayoutPanel1);
+            Name = "Form1";
+            Text = "Word文件查找和替换工具";
+            Load += Form1_Load;
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private Button button2;
+        private ListView listView1;
+        private TextBox textBox4;
+        private Button button3;
+        private Button button4;
+        private Label label3;
+        private Label label4;
+        private ProgressBar progressBar1;
+        private Label label5;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader3;
+        private TableLayoutPanel tableLayoutPanel1;
+        public TextBox textBox3;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,464 @@
+﻿using System.Text.RegularExpressions;
+using System.Runtime.InteropServices;
+using Microsoft.Office.Interop.Word;
+using System;
+using System.IO;
+using System.Windows.Forms;
+using Application = Microsoft.Office.Interop.Word.Application;
+
+namespace DocReplacer
+{
+    public partial class Form1 : Form
+    {
+        // 初始化窗体组件
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 调用Windows目录选择器获取文件夹路径
+        /// </summary>
+        /// <returns>用户选择的文件夹路径</returns>
+        private string SelectFolder()
+        {
+            using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
+            {
+                // 设置初始目录为桌面
+                folderDialog.SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                folderDialog.Description = "请选择目录";
+
+                // 返回用户选择的路径或空字符串
+                return folderDialog.ShowDialog() == DialogResult.OK ? folderDialog.SelectedPath : string.Empty;
+            }
+        }
+
+        // 线程安全的进度更新方法
+        private void UpdateProgress(int processed, int total, string currentFile = "")
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action(() => UpdateProgress(processed, total, currentFile)));
+                return;
+            }
+
+            // 更新进度条
+            progressBar1.Maximum = total;
+            progressBar1.Value = processed;
+
+            // 可选：更新状态标签
+            // labelStatus.Text = $"正在处理：{currentFile} ({processed}/{total})";
+
+            // 强制刷新界面
+            //Application.DoEvents();
+        }
+
+        #region 按钮点击事件
+        // 源目录选择按钮
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = SelectFolder(); // 将选择的路径显示到源目录文本框
+        }
+
+        // 目标目录选择按钮
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = SelectFolder(); // 将选择的路径显示到目标目录文本框
+        }
+
+        // 执行替换按钮
+
+        // 查找按钮点击事件
+        private async void button3_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                button3.Enabled = false;
+                listView1.Items.Clear(); // 清空旧数据
+                progressBar1.Value = 0; // 重置进度条
+                // 验证输入
+                if (!ValidateSearchInputs()) return;
+
+                // 获取所有查找规则
+                var searchKeywords = textBox3.Lines;
+
+                // 获取源目录所有Word文件
+                // 修改后（支持doc和docx）
+                var sourceFiles = Directory.GetFiles(textBox1.Text, "*.*")
+                    .Where(f => f.EndsWith(".doc", StringComparison.OrdinalIgnoreCase) ||
+                                f.EndsWith(".docx", StringComparison.OrdinalIgnoreCase))
+                    .ToArray();
+
+                // 设置进度条最大值
+                UpdateProgress(0, sourceFiles.Length, "准备开始");
+
+                // 异步执行查找
+                await System.Threading.Tasks.Task.Run(() => SearchFiles(sourceFiles, searchKeywords));
+
+                MessageBox.Show("查找完成！");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"查找失败：{ex.Message}");
+            }
+            finally
+            {
+                button3.Enabled = true;
+            }
+
+        }
+
+        private async void button4_Click(object sender, EventArgs e)
+        {
+            //dynamic wordApp = Activator.CreateInstance(Type.GetTypeFromProgID("Word.Application"));
+            try
+            {
+                button4.Enabled = false; // 禁用按钮防止重复点击
+                progressBar1.Value = 0; // 重置进度条
+
+
+                // 验证输入有效性
+                if (!ValidateInputs()) return;
+
+                // 获取所有替换规则（支持多行）
+                var replaceRules = ParseReplaceRules();
+
+                // 获取源目录所有Word文件
+                var sourceFiles = Directory.GetFiles(textBox1.Text, "*.*")
+                    .Where(f => f.EndsWith(".doc", StringComparison.OrdinalIgnoreCase) ||
+                    f.EndsWith(".docx", StringComparison.OrdinalIgnoreCase))
+                    .ToArray();
+
+                // 设置进度条最大值
+                UpdateProgress(0, sourceFiles.Length, "准备开始");
+
+                // 异步执行替换操作
+                await System.Threading.Tasks.Task.Run(() => ProcessFiles(sourceFiles, textBox2.Text, replaceRules));
+
+                MessageBox.Show("文件替换完成！");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"操作失败：{ex.Message}");
+            }
+            finally
+            {
+                button4.Enabled = true; // 重新启用按钮
+            }
+        }
+        #endregion
+
+        /// <summary>
+        /// 批量处理文件的核心方法
+        /// </summary>
+        /// <param name="sourceFiles">源文件路径数组</param>
+        /// <param name="targetFolder">目标保存目录</param>
+        /// <param name="replaceRules">替换规则集合</param>
+        private void ProcessFiles(string[] sourceFiles, string targetFolder, Tuple<string, string>[] replaceRules)
+        {
+            int totalFiles = sourceFiles.Length;
+            int processedCount = 0;
+
+            Application wordApp = null;
+            try
+            {
+                // 创建Word应用实例
+                wordApp = new Application
+                {
+                    Visible = false,
+                    ScreenUpdating = false,
+                    DisplayAlerts = WdAlertLevel.wdAlertsNone
+                };
+
+                foreach (var filePath in sourceFiles)
+                {
+
+                    // 生成目标路径
+                    var targetPath = Path.Combine(targetFolder, Path.GetFileName(filePath));
+
+                    // 更新进度
+                    Interlocked.Increment(ref processedCount);
+                    UpdateProgress(processedCount, totalFiles, Path.GetFileName(filePath));
+
+                    Document doc = null;
+                    try
+                    {
+
+                        // 打开文档
+                        doc = wordApp.Documents.Open(filePath, ReadOnly: false, Visible: false);
+
+                        // 应用所有替换规则
+                        foreach (var rule in replaceRules)
+                        {
+                            ReplaceText(doc, rule.Item1, rule.Item2);
+                        }
+
+                        // 另存到新位置
+                        // 修改后的保存逻辑
+                        doc.SaveAs2(
+                            FileName: targetPath,
+                            FileFormat: Path.GetExtension(targetPath).ToLower() == ".doc" ?
+                                WdSaveFormat.wdFormatDocument : WdSaveFormat.wdFormatDocumentDefault
+                        );
+                        doc.Close(SaveChanges: false);
+                    }
+                    catch (COMException ex) // 新增的异常捕获
+                    {
+                        this.Invoke((MethodInvoker)delegate
+                        {
+                            MessageBox.Show($"无法打开文件 {Path.GetFileName(filePath)}：{ex.Message}");
+                        });
+                        continue; // 跳过当前文件
+                    }
+                    finally
+                    {
+                        // 释放COM对象
+                        if (doc != null) Marshal.ReleaseComObject(doc);
+                    }
+                }
+            }
+            finally
+            {
+                // 清理Word进程
+                if (wordApp != null)
+                {
+                    wordApp.Quit(SaveChanges: false);
+                    Marshal.ReleaseComObject(wordApp);
+                    GC.Collect();
+                    GC.WaitForPendingFinalizers();
+                }
+            }
+        }
+
+        /// <summary>
+        /// 在单个文档中查找关键词并返回上下文
+        /// </summary>
+        private List<string> FindKeywordInDocument(Document doc, string keyword)
+        {
+            var results = new List<string>();
+            Microsoft.Office.Interop.Word.Range range = doc.Content;
+            Find find = range.Find;
+
+            try
+            {
+                find.ClearFormatting();
+                find.Text = keyword;
+                find.MatchCase = false;
+                find.MatchWholeWord = false;
+
+                while (find.Execute())
+                {
+                    if (range.Find.Found)
+                    {
+                        // 获取匹配段落全文作为上下文
+                        string context = range.Paragraphs[1].Range.Text.Trim();
+                        results.Add(context);
+                    }
+                }
+            }
+            finally
+            {
+                Marshal.ReleaseComObject(find);
+                Marshal.ReleaseComObject(range);
+            }
+
+            return results;
+        }
+        /// <summary>
+        /// 批量查找文档内容
+        /// </summary>
+        private void SearchFiles(string[] sourceFiles, string[] keywords)
+        {
+            int totalFiles = sourceFiles.Length;
+            int processedCount = 0;
+
+            Application wordApp = null;
+            try
+            {
+                wordApp = new Application
+                {
+                    Visible = false,
+                    ScreenUpdating = false,
+                    DisplayAlerts = WdAlertLevel.wdAlertsNone
+                };
+
+                foreach (var filePath in sourceFiles)
+                {
+                    // 更新进度
+                    Interlocked.Increment(ref processedCount);
+                    UpdateProgress(processedCount, totalFiles, Path.GetFileName(filePath));
+
+                    Document doc = null;
+                    try
+                    {
+                        doc = wordApp.Documents.Open(filePath, ReadOnly: true, Visible: false);
+
+                        foreach (var keyword in keywords)
+                        {
+                            if (string.IsNullOrWhiteSpace(keyword)) continue;
+
+                            var foundItems = FindKeywordInDocument(doc, keyword.Trim());
+                            if (foundItems.Count > 0)
+                            {
+                                // 跨线程更新UI
+                                this.Invoke((MethodInvoker)delegate
+                                {
+                                    foreach (var item in foundItems)
+                                    {
+                                        var listItem = new ListViewItem(Path.GetFileName(filePath));
+                                        listItem.SubItems.Add(keyword);
+                                        listItem.SubItems.Add(item);
+                                        listView1.Items.Add(listItem);
+                                    }
+                                });
+                            }
+                        }
+                        doc.Close(SaveChanges: false);
+                    }
+                    catch (COMException ex) // 新增的异常捕获
+                    {
+                        this.Invoke((MethodInvoker)delegate
+                        {
+                            MessageBox.Show($"无法打开文件 {Path.GetFileName(filePath)}：{ex.Message}");
+                        });
+                        continue; // 跳过当前文件
+                    }
+                    finally
+                    {
+                        if (doc != null) Marshal.ReleaseComObject(doc);
+                    }
+                }
+            }
+            finally
+            {
+                if (wordApp != null)
+                {
+                    wordApp.Quit(SaveChanges: false);
+                    Marshal.ReleaseComObject(wordApp);
+                    GC.Collect();
+                    GC.WaitForPendingFinalizers();
+                }
+            }
+        }
+        /// <summary>
+        /// 在文档中执行文本替换
+        /// </summary>
+        private void ReplaceText(Document doc, string oldText, string newText)
+        {
+            var range = doc.Content;
+            var find = range.Find;
+
+            try
+            {
+                // 配置查找参数
+                find.ClearFormatting();
+                find.Replacement.ClearFormatting();
+
+                // 执行全部替换
+                find.Execute(
+                    FindText: oldText,
+                    ReplaceWith: newText,
+                    Replace: WdReplace.wdReplaceAll,
+                    MatchCase: false,
+                    MatchWholeWord: false
+                );
+            }
+            finally
+            {
+                // 释放COM对象
+                Marshal.ReleaseComObject(find);
+                Marshal.ReleaseComObject(range);
+            }
+        }
+
+        /// <summary>
+        /// 解析替换规则（支持多行）
+        /// </summary>
+        private Tuple<string, string>[] ParseReplaceRules()
+        {
+            // 获取两个文本框的内容
+            var findLines = textBox3.Lines;
+            var replaceLines = textBox4.Lines;
+
+            // 验证规则数量匹配
+            if (findLines.Length != replaceLines.Length)
+                throw new ArgumentException("查找文本与被替换文本行数不匹配");
+
+            // 创建规则数组
+            var rules = new Tuple<string, string>[findLines.Length];
+            for (int i = 0; i < findLines.Length; i++)
+            {
+                rules[i] = Tuple.Create(findLines[i].Trim(), replaceLines[i].Trim());
+            }
+
+            return rules;
+        }
+
+        /// <summary>
+        /// 验证用户输入有效性
+        /// </summary>
+        private bool ValidateInputs()
+        {
+            // 验证目录路径
+            if (string.IsNullOrWhiteSpace(textBox1.Text) ||
+                string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                MessageBox.Show("请先选择源目录和目标目录");
+                return false;
+            }
+
+            // 验证目录存在性
+            if (!Directory.Exists(textBox1.Text))
+            {
+                MessageBox.Show("源目录不存在");
+                return false;
+            }
+
+            // 验证规则输入
+            if (textBox3.Lines.Length == 0 ||
+                textBox4.Lines.Length == 0)
+            {
+                MessageBox.Show("请输入替换的文本");
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// 验证查找操作的输入
+        /// </summary>
+        private bool ValidateSearchInputs()
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                MessageBox.Show("请先选择源目录");
+                return false;
+            }
+
+            if (!Directory.Exists(textBox1.Text))
+            {
+                MessageBox.Show("源目录不存在");
+                return false;
+            }
+
+            if (textBox3.Lines.Length == 0 || string.IsNullOrWhiteSpace(textBox3.Text))
+            {
+                MessageBox.Show("请输入要查找的文本");
+                return false;
+            }
+
+            return true;
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("1.在使用工具查找或替换前关闭所有word进程！\n2.使用替换和查找期间不要使用word进行操作！\n3.查找和替换执行期间不要中途中断任务，如果中断任务需要自行清理内存中的word进程！");
+        }
+    }
+}
